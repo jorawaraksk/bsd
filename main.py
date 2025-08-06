8f0f682f2d84c68e15aceb91420490f8c223ecea
rohanh6609 <rohanh6609@gmail.com>
2025-08-06 07:43:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ if __name__ == "__main__" :
     bot = Client(
         "StarkBot",
         bot_token=os.environ.get("BOT_TOKEN"),
-        api_id=int(os.environ.get("API_ID")),
+        api_id=os.environ.get("API_ID"),
         api_hash=os.environ.get("API_HASH"),
         sleep_threshold=20,
         plugins=plugins,
@@ -68,3 +68,4 @@ if __name__ == "__main__" :
     
     asyncio.get_event_loop().run_until_complete(main())
     LOGGER.info(f"<---Bot Stopped-->")
+
